8d7b3a4d9019677e87a67c5d25624dcdebe3b4ef
Zafar Hasanov <zaferhasanov@gmail.com>
2020-03-02 00:10:34 +0400

    List transactions story implemented. Acceptance criteria - order by date and view details - are met. Dummy data imported via resource/data.sql file

---
+++ pbd-ms-transaction/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO TRANSACTION  (id, account_number, amount, description,  insert_date, user_name) VALUES (
+1, 'abckasd1291',100, 'Amazon.com Book order','2020-01-27 02:25:13.888','admin@admin.com');
+
+INSERT INTO TRANSACTION  (id, account_number, amount, description,  insert_date, user_name) VALUES (
+2, 'abckasd1221',459, 'Zolonda.com shoes order','2019-01-27 02:25:13.888','admin@admin.com');
